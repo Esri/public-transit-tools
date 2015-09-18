528ffe9084001c8718462e54696cf7e48f3088e8
mmorang <mmorang@esri.com>
2015-09-18 14:48:28 -0700

    Added required copyright info to script code

--- display-GTFS-route-shapes/scripts/DisplayGTFSRouteShapes.py
+++ display-GTFS-route-shapes/scripts/DisplayGTFSRouteShapes.py
@@ -3,11 +3,22 @@
 ## Created by: Melinda Morang, Esri, mmorang@esri.com
 ## Last updated: 11 June 2015
 ############################################################################
-
 ''' Display GTFS Route Shapes
 Display GTFS Route Shapes converts GTFS route and shape data into an ArcGIS
 feature class so you can visualize your GTFS routes on a map.
 '''
+################################################################################
+'''Copyright 2015 Esri
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.'''
+################################################################################
 
 import sqlite3, operator, os
 import arcpy
--- display-GTFS-route-shapes/scripts/sqlize_csv.py
+++ display-GTFS-route-shapes/scripts/sqlize_csv.py
@@ -1,6 +1,18 @@
+################################################################################
 # sqlize_csv.py, originally written by Luitien Pan
-# Last updated 8 June 2015 by Melinda Morang
-
+# Last updated 8 June 2015 by Melinda Morang, Esri
+################################################################################
+'''Copyright 2015 Esri
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.'''
+################################################################################
 # Imports the CSV-formatted GTFS information into a SQLite database file.
 # Handles data conversion, table creation, and indexing transparently.
 #
