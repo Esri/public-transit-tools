2169f230e6899ec6d763522e3015b77c589b6e8f
Melinda Morang <mmorang@esri.com>
2023-08-29 08:26:48 -0700

    Add geometry checks

--- transit-network-analysis-tools/unittests/test_CreatePercentAccessPolygons_tool.py
+++ transit-network-analysis-tools/unittests/test_CreatePercentAccessPolygons_tool.py
@@ -1,5 +1,11 @@
 """Unit tests for the Create Percent Access Polygons script tool.
 
+The input data for these tests is made up and intended to be simple to verify.
+The tests don't attempt to compare geometry exactly because small changes to ArcGIS Pro could cause those checks to be
+flaky over time.  They just ensure that the output is generally correct.  If you're going to make significant changes to
+the logic of this tool, you will likely need to create some more exhaustive tests that compare the actual
+output geometry before and after the changes.
+
 Copyright 2023 Esri
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -44,6 +50,24 @@ class TestCreatePercentAccessPolygonsTool(unittest.TestCase):
         self.output_gdb = os.path.join(self.scratch_folder, "outputs.gdb")
         arcpy.management.CreateFileGDB(os.path.dirname(self.output_gdb), os.path.basename(self.output_gdb))
 
+    def check_output(self, out_fc, expected_percents):
+        """Check the output feature class."""
+        self.assertTrue(arcpy.Exists(out_fc))
+        self.assertEqual(len(expected_percents), int(arcpy.management.GetCount(out_fc).getOutput(0)))
+        self.assertIn("Percent", [f.name for f in arcpy.ListFields(out_fc)])
+        actual_percents = []
+        for row in arcpy.da.SearchCursor(out_fc, "Percent"):
+            actual_percents.append(row[0])
+        actual_percents = sorted(actual_percents)
+        self.assertEqual(expected_percents, actual_percents)
+
+    def make_percent_shape_dict(self, out_fc):
+        """Make a dictionary of {percent: shape geometry} for the output feature class."""
+        shapes = {}
+        for row in arcpy.da.SearchCursor(out_fc, ["Percent", "SHAPE@"]):
+            shapes[row[0]] = row[1]
+        return shapes
+
     def test_tool(self):
         """Test the tool."""
         in_fc = os.path.join(self.in_gdb, "TimeLapsePolys_1Fac_1Cutoff")
@@ -58,9 +82,28 @@ class TestCreatePercentAccessPolygonsTool(unittest.TestCase):
             out_fc_th,
             [50, 75]
         )
-        self.assertTrue(arcpy.Exists(out_fc))
-        self.assertTrue(arcpy.Exists(out_fc_th))
-        self.assertEqual(2, int(arcpy.management.GetCount(out_fc_th).getOutput(0)))
+
+        # Do some basic checks of outputs
+        all_percents = [25, 50, 75, 100]
+        self.check_output(out_fc, all_percents)
+        self.check_output(out_fc_th, [50, 75])
+
+        # Check the relationships of the shapes in the output
+        out_shapes = self.make_percent_shape_dict(out_fc)
+        th_shapes = self.make_percent_shape_dict(out_fc_th)
+        # For the main output, none of the polygons should overlap.
+        for percent1 in all_percents:
+            shape1 = out_shapes[percent1]
+            for percent2 in [p for p in all_percents if p > percent1]:
+                shape2 = out_shapes[percent2]
+                self.assertFalse(shape1.overlaps(shape2), "Shapes should not overlap.")
+        # For the threshold output, the 75% polygon should be fully contained within the 50% polygon
+        self.assertTrue(th_shapes[50].contains(th_shapes[75]), "Smaller threshold polygon should contain larger.")
+        # The threshold polygons should contain all the main polygons of larger percentages
+        for percent in [50, 75, 100]:
+            self.assertTrue(th_shapes[50].contains(out_shapes[percent]))
+        for percent in [75, 100]:
+            self.assertTrue(th_shapes[75].contains(out_shapes[percent]))
 
 
 if __name__ == '__main__':
